4e2d129316ac35a145cf8ae5a4040e7fb89f0d0b
Laisfsnc <lais.2023303415@aluno.iffar.edu.br>
2025-03-31 14:43:45 -0300

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP</title>
+</head>
+<body>
+<?php
+   $_txtnome=$_GET ["txtnome"];
+echo "Seu nome: $_txtnome";
+    ?>
+</body>
+</html>
