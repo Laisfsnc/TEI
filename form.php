c54fae17dca5cb4c49a3bd3f0c4f3159aa9b3dce
Laisfsnc <lais.2023303415@aluno.iffar.edu.br>
2025-04-14 13:57:42 -0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<html>
+
+<body>
+
+    <form action="form.php" method="post">
+
+        Digite seu nome:
+        <input type="text" size="80" name="txtnome">
+        <input type="submit" name="enviar" value="Vai!">
+        <input type="submit" name="retirar" value="Remove">
+
+    </form>
+
+    <?php
+    session_start();
+    if (!isset($_SESSION['conv'])) {
+        $_SESSION['conv'] = [];
+    }
+    if (isset($_POST["enviar"])) {
+        $nome = $_POST["txtnome"];
+
+        array_push($_SESSION['conv'], $nome);
+
+        foreach ($_SESSION['conv'] as $i => $v) {
+            echo "($i) - $v <br>";
+        }
+    }
+    if (isset($_POST['retirar'])) {
+        array_pop($_SESSION['conv']);
+
+        foreach ($_SESSION['conv'] as $i => $v) {
+            echo "($i) - $v <br>";
+        }
+    }
+
+
+    ?>
+
+</body>
+
+
+</html>
